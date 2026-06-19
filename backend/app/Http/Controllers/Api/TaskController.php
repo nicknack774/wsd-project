f513c11200a4e84cb051c58f75915cc14b9b8efe
Nihal Singh <nihalpratapsingh774@gmail.com>
2026-06-19 23:28:18 +0200

    Project 3: add PostgreSQL CRUD tasks API

---
+++ backend/app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class TaskController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $tasks = Task::all();
+
+        return response()->json($tasks, 200);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'title' => 'required|string',
+            'description' => 'nullable|string',
+            'status' => 'nullable|string',
+            'album_number' => 'required|string',
+        ]);
+
+        $task = Task::create($validated);
+
+        return response()->json($task, 201);
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $task = Task::findOrFail($id);
+
+        return response()->json($task, 200);
+    }
+
+    public function update(Request $request, string $id): JsonResponse
+    {
+        $task = Task::findOrFail($id);
+
+        $validated = $request->validate([
+            'title' => 'sometimes|required|string',
+            'description' => 'nullable|string',
+            'status' => 'nullable|string',
+            'album_number' => 'sometimes|required|string',
+        ]);
+
+        $task->update($validated);
+
+        return response()->json($task, 200);
+    }
+
+    public function destroy(string $id): JsonResponse
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,11 @@ use App\Http\Controllers\EchoController;
 
 Route::get('/echo', [EchoController::class, 'echo']);
 Route::post('/echo', [EchoController::class, 'echo']);
+
+use App\Http\Controllers\Api\TaskController;
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks/{id}', [TaskController::class, 'show']);
+Route::put('/tasks/{id}', [TaskController::class, 'update']);
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
